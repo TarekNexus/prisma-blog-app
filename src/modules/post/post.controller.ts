55a39b3d0eb8cff7b88b10f1fec6f249afdbdf44
TarekNexus <muhammadtarek5920@gmail.com>
2026-01-02 22:26:57 +0600

    setup done

---
+++ src/modules/post/post.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { postService } from "./post.service";
+
+const createPost = async (req: Request, res: Response) => {
+    try {
+        const user = req.user;
+        if (!user) {
+            return res.status(400).json({
+                error: "Unauthorized!",
+            })
+        }
+        const result = await postService.createPost(req.body, user.id as string)
+        res.status(201).json(result)
+    } catch (e) {
+        res.status(400).json({
+            error: "Post creation failed",
+            details: e
+        })
+    }
+}
+
+const getAllPosts = async (req: Request, res: Response) => {
+    try {
+        const result = await postService.getAllPosts(); 
+        res.status(200).json(result);
+    } catch (e) {
+        res.status(400).json({
+            error: "post creation failed",
+            details: e
+        });
+    }
+
+
+}
+
+export const PostController = {
+    createPost,
+    getAllPosts
+}
